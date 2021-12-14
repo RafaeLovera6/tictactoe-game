787863a890f71c6ed5223087287f828aef051823
RafaeLovera6 <rafaelovera6@gmail.com>
2021-12-14 05:47:13 +0000

    missing the final logic

--- src/app.js
+++ src/app.js
@@ -4,8 +4,18 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
+let board = [null, null, null, null, null, null, null, null, null];
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  document.querySelector(".board").innerHTML = board
+
+    .map(elm => drawGrid(elm))
+    .join("");
 };
+
+function drawGrid(symbol = "times") {
+  return `<div class="grid">
+  <i class="${symbol}"></i>
+</div>`;
+}
