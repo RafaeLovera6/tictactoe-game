6e70719ab0ca2bd3a436a69174d09913a91a2c34
RafaeLovera6 <rafaelovera6@gmail.com>
2021-12-16 00:27:46 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -4,18 +4,36 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+//write your code here
 let board = [null, null, null, null, null, null, null, null, null];
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-  document.querySelector(".board").innerHTML = board
+let currentPlayer = "times";
 
+window.onload = render;
+
+function render() {
+  document.querySelector(".board").innerHTML = board
     .map(elm => drawGrid(elm))
     .join("");
-};
+
+  document.querySelectorAll(".grid").forEach((node, index) =>
+    node.addEventListener("click", function() {
+      play(index);
+    })
+  );
+}
+
+function play(index) {
+  board[index] = currentPlayer;
+  if (currentPlayer === "times") {
+    currentPlayer = "circle";
+  } else {
+    currentPlayer = "times";
+  }
+  render();
+}
 
 function drawGrid(symbol = "times") {
   return `<div class="grid">
-  <i class="${symbol}"></i>
+  <i class="fas fa-${symbol}"></i>
 </div>`;
 }
